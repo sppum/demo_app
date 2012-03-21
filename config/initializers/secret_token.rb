c1df49b5ac75465d29564d2e617dd250293553e2
Chris King <chris@wmnc.org>
2012-03-21 21:30:41 +0000

    Ran 'rails new demo_app this time'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '6b861170bf419913efd4947be8e88e016a492e665f1421370a5952f4f3e0bde4ec23fbd1c9c0cae891cab4304fdba88275665edfbc7f2825516b06e959211cc9'
